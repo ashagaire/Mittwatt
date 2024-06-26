d5f20d51613230fb24def4ed42cf02efddbfce51
Ahmed <ahmed.massod@gmail.com>
2024-06-26 13:49:38 +0300

    Add AveragePrices component

---
+++ src/components/AveragePrices.tsx
@@ -0,0 +1,86 @@
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+
+interface AveragePricesProps {
+  startDate: Date;
+  endDate: Date;
+  data: any;
+}
+
+const AveragePrices: React.FC<AveragePricesProps> = ({
+  startDate,
+  endDate,
+  data,
+}) => {
+  return (
+    <>
+      <div className="flex w-full items-center justify-center pt-20 text-center text-2xl  text-black">
+        Average price from{" "}
+        <span className="mx-2 text-green-800">
+          {" "}
+          {startDate.toDateString()}{" "}
+        </span>{" "}
+        to{" "}
+        <span className="mx-2 text-green-800"> {endDate.toDateString()} </span>
+      </div>
+      <div className="h-96 w-full px-10">
+        <ResponsiveContainer>
+          <LineChart
+            data={data}
+            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+          >
+            <Line type="monotone" dataKey="price" stroke="#16A34A" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis
+              dataKey="date"
+              tickFormatter={(value) => {
+                return value;
+              }}
+            />
+            <YAxis />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="flex w-full items-center justify-center pl-32 pr-32 pt-10 text-black">
+        <table className="min-w-full table-auto border-collapse border border-gray-500 px-10">
+          <thead>
+            <tr className="bg-green-600 text-white">
+              <th className="border border-gray-500 px-4 py-2 ">Date</th>
+              <th className="border border-gray-500 px-4 py-2 ">
+                Average Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data && data.length > 0 ? (
+              data.map((item: any, index: number) => (
+                <tr key={index} className="bg-green-50">
+                  <td className="border border-gray-500 px-4 py-2 text-center">
+                    {item.date}
+                  </td>
+                  <td className="border border-gray-500 px-4 py-2 text-center">
+                    {item.price?.toFixed(2) + " c/kWh"}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <td colSpan={3} className="border border-gray-500 px-4 py-2">
+                No data available
+              </td>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default AveragePrices;
--- src/pages/future.tsx
+++ src/pages/future.tsx
@@ -4,24 +4,15 @@
 
 import { useMemo } from "react";
 import { api } from "~/utils/api";
-
-import {
-  ResponsiveContainer,
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-} from "recharts";
+import AveragePrices from "~/components/AveragePrices";
 
 export default function Future() {
   const startDate = useMemo(
-    () => new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+    () => new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
     [],
   );
   const endDate = useMemo(
-    () => new Date(Date.now() + 14 * 24 * 60 * 60 * 1000),
+    () => new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     [],
   );
   const { data, error, isLoading } =
@@ -49,64 +40,8 @@ export default function Future() {
       <div className="w-full pb-6 pt-6 text-center text-4xl font-bold">
         Future Prices
       </div>
-      <div className="flex w-full items-center justify-center pt-20 text-center text-2xl  text-black">
-        Average price from{" "}
-        <span className="mx-2 text-green-800">
-          {" "}
-          {startDate.toDateString()}{" "}
-        </span>{" "}
-        to{" "}
-        <span className="mx-2 text-green-800"> {endDate.toDateString()} </span>
-      </div>
-      <div className="h-96 w-full px-10">
-        <ResponsiveContainer>
-          <LineChart
-            data={data}
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-          >
-            <Line type="monotone" dataKey="price" stroke="#16A34A" />
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis
-              dataKey="date"
-              tickFormatter={(value) => {
-                return value;
-              }}
-            />
-            <YAxis />
-            <Tooltip />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-      <div className="flex w-full items-center justify-center pl-32 pr-32 pt-10 text-black">
-        <table className="min-w-full table-auto border-collapse border border-gray-500 px-10">
-          <thead>
-            <tr className="bg-green-600 text-white">
-              <th className="border border-gray-500 px-4 py-2 ">Date</th>
-              <th className="border border-gray-500 px-4 py-2 ">
-                Average Price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data && data.length > 0 ? (
-              data.map((item: any, index: number) => (
-                <tr key={index} className="bg-green-50">
-                  <td className="border border-gray-500 px-4 py-2 text-center">
-                    {item.date}
-                  </td>
-                  <td className="border border-gray-500 px-4 py-2 text-center">
-                    {item.price?.toFixed(2) + " c/kWh"}
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <td colSpan={3} className="border border-gray-500 px-4 py-2">
-                No data available
-              </td>
-            )}
-          </tbody>
-        </table>
-      </div>
+
+      <AveragePrices startDate={startDate} endDate={endDate} data={data} />
     </div>
   );
 }
