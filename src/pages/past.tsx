74e71382206df1d8cbdd59196f10a642e9538212
Ahmed <ahmed.massod@gmail.com>
2024-06-26 13:23:33 +0300

    future prices endpoint and basic frontend

--- src/pages/past.tsx
+++ src/pages/past.tsx
@@ -95,7 +95,7 @@ export default function Past() {
                     {item.date}
                   </td>
                   <td className="border border-gray-500 px-4 py-2 text-center">
-                    {item.price.toFixed(2) + " c/kWh"}
+                    {item.price?.toFixed(2) + " c/kWh"}
                   </td>
                 </tr>
               ))
